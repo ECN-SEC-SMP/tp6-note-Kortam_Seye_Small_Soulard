008c8757306c4cae6a7ea2793e9bb94c5af4f711
Milo <milosoulardgeii@gmail.com>
2025-01-14 15:07:36 +0100

    début de code dans main et plateau

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "plateau.h"
+#include "case.h"
+#include "joueur.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+using namespace std;
+
+int main() {
+    int nombreJoueurs;
+    cout << "Entrez le nombre de joueurs: ";
+    cin >> nombreJoueurs;
+
+    vector<Joueur> joueurs;
+    for (int i = 0; i < nombreJoueurs; ++i) {
+        string nom;
+        cout << "Entrez le nom du joueur " << i + 1 << ": ";
+        cin >> nom;
+        joueurs.emplace_back(nom);
+    }
+
+    // Mélanger l'ordre des joueurs
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    shuffle(joueurs.begin(), joueurs.end(), default_random_engine(seed));
+
+    cout << "Ordre de passage des joueurs:" << endl;
+    for (const auto& joueur : joueurs) {
+        cout << joueur.getNom() << endl;
+    }
+
+
+    Plateau plateau;
+    plateau.initialiser(joueurs);
+    plateau.commencerJeu();
+
+    return 0;
+}
